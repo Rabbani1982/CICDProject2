b51debd5a6441338b03d1491c15ff3b2ac980fcf
Rabbani1982 <syedraqa@gmail.com>
2025-07-30 08:20:07 +0530

    CICD Second commit

---
+++ src/test/java/CICDpackage/CICD2.java
@@ -0,0 +1,10 @@
+package CICDpackage;
+
+public class CICD2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/CICDpackage/CICD3.java
@@ -0,0 +1,10 @@
+package CICDpackage;
+
+public class CICD3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
