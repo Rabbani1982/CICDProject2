fc321bbda56ce453e1e1fffbe8bf56c40f646def
Rabbani1982 <syedraqa@gmail.com>
2025-07-30 08:46:00 +0530

    Thired commit

--- src/test/java/CICDpackage/CICD2.java
+++ src/test/java/CICDpackage/CICD2.java
@@ -2,8 +2,9 @@ package CICDpackage;
 
 public class CICD2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
